762e260f79c66da0372d255599efe1b95002ff7e
pallamidessi <joseph.pallamidessi@gmail.com>
2015-10-29 13:05:47 +0100

    Finish 2opt*-pd !

--- 2OptStar.cpp
+++ 2OptStar.cpp
@@ -75,15 +75,18 @@ void toTail(array<FareVector, MAX_NB_VEHICLE>& sIndiv, int src, int dest,
 
 void swapEdge(Individual& indiv, Individual& result, int v1, int v2,
               int pos1, int pos2) {
+
     array<FareVector, MAX_NB_VEHICLE> sIndiv;
     splitIndividual(indiv, sIndiv);
 
     int i;
 
+    /* Swap the until the end of the shortest route */
     for (i = 0; i < std::min(sIndiv[v1].size, sIndiv[v2].size); i++) {
         std::swap(sIndiv[v1][pos1 + i], sIndiv[v2][pos2 + i]);
     }
 
+    /* Move the remaining tail to the other route */
     if(sIndiv[v1].size > sIndiv[v2].size) {
         toTail(sIndiv, v1, v2, i);
     } else if (sIndiv[v1].size < sIndiv[v2].size) {
@@ -93,142 +96,137 @@ void swapEdge(Individual& indiv, Individual& result, int v1, int v2,
     stitchIndividual(result, sIndiv);
 }
 
-void twoOptStar() {
+Fitness evaluateRouteFromPoint(Individual& indiv,
+                               const cube_matrix& cube,
+                               const Options& options,
+                               int offset,
+                               size_t remainingElement,
+                               int costPerKm,
+                               int vehicleIdx) {
+    Fitness base;
+
+    base[COST] = costFitness(&indiv.listFare[offset], remainingElement, data,
+                             costPerKm, cube, vehicleIdx, indiv, options, NORMAL);
+    base[DELAY] = delayFitness(&indiv.listFare[offset], remainingElement, data,
+                               costPerKm, cube, vehicleIdx, indiv, options, NORMAL);
+    base[LOAD] = loadFitness(&indiv.listFare[offset], remainingElement, data,
+                             costPerKm, costPerKm, cube, vehicleIdx, indiv,options, NORMAL);
+
+    return base;
+}
+
+void twoOptStar(Individual& indiv, const Data& data, const Options& options) {
     bool improvement = true;
 
     while (improvement) {
+        improvement = false;
+
+        /* Iterate through the vehicle as base vehicle */
         for(size_t v = 0; v < std::min(indiv.vehiclesCount.size(), maxNbVehicle); ++v) {
-            for(int i = pos; i < pos + indiv.vehiclesCount[v]; i++) {
+            auto vehicleI = data.vehicle(v);
+
+            /* Iterate through the edges of the base vehicle*/
+            for(int i = offsetBase; i < offsetBase + indiv.vehiclesCount[v]; i++) {
+                auto fares& = indiv.listFares;
+
                 /* Only exchange adjacent plannedElement */
                 if ((fares[i].journeyId != fares[i + 1].journeyId)
                         && (fares[i + 2].journeyId != fares[i + 3].journeyId))
                     continue;
 
-                // Compute old cost of the two route from the changing point
-                //from i
-
+                /* Compute old cost of the base route from the changing point */
                 Fitness base;
+                base = evaluateRouteFromPoint(indiv, cube, options, i,
+                                              (vehiclesCount[v] + offsetBase) - i, vehicleI.costPerKm, v);
 
-                base[COST]  = costFitness(&fares[i],
-                                          (vehiclesCount[v] + pos) - i,
-                                          data,
-                                          costPerKm,
-                                          cube,
-                                          vehicleIdx,
-                                          indiv,
-                                          options,
-                                          NORMAL);
-
-                base[DELAY] = delayFitness(&fares[i],
-                                           (vehiclesCount[v] + pos) - i,
-                                           data,
-                                           costPerKm,
-                                           cube,
-                                           vehicleIdx,
-                                           indiv,
-                                           options,
-                                           NORMAL);
-
-                base[LOAD]  = loadFitness(&fares[i],
-                                          (vehiclesCount[v] + pos) - i,
-                                          data,
-                                          costPerKm,
-                                          cube,
-                                          vehicleIdx,
-                                          indiv,
-                                          options,
-                                          NORMAL);
-
+                /* Iterate through the vehicles as the second one */
                 for(size_t v2 = 0; v2 < std::min(indiv.vehiclesCount.size(), maxNbVehicle);
                         ++v2) {
 
-                    // Different vehicle type
-                    if (data.vehicle(v2).type != data.vehicle(v).type) {
+                    auto vehicleII = data.vehicle(v2);
+
+                    /* Different vehicle type */
+                    if (vehicleI.type != vehicleII.type) {
                         continue;
                     }
 
-                    for(int j = pos2; j < pos2 + indiv.vehiclesCount[v2]; j++) {
+                    /* Same vehicle*/
+                    if (v != v2) {
+                        continue;
+                    }
 
-                        if ((vehiclesCount[v2] + pos2) - j < 4) {
+                    for(int j = offset; j < offset + indiv.vehiclesCount[v2]; j++) {
+
+                        /* Not enough plannedElement in the route to attempt local
+                         * optimisation */
+                        if ((vehiclesCount[v2] + offset) - j < 4) {
                             break;
                         }
 
-                        /* Only exchange adjacent plannedElement */
+                        /* Only exchange adjacent plannedElement: the pick-up
+                         * element must be next to the delivery */
                         if ((fares[j].journeyId != fares[j + 1].journeyId)
                                 && (fares[j + 2].journeyId != fares[j + 3].journeyId))
                             continue;
 
-                        // Time window
+                        /* At this point in the vehicle the temporal order must
+                         * respected */
                         if (fares[j + 2].realTimestamp < fares[i + 1].realTimestamp) {
                             continue;
                         }
 
 
-                        Fitness newGC;
-                        Fitness baseCpy = base;
-                        //from j
-                        baseCpy[DELAY] += delayFitness(&fares[j],
-                                                       (vehiclesCount[v2] + pos2) - j,
-                                                       data,
-                                                       costPerKm,
-                                                       cube,
-                                                       vehicleIdx,
-                                                       indiv,
-                                                       options,
-                                                       NORMAL);
+                        /* Compute old cost of the second route from its changing point */
+                        Fitness oldValue = evaluateRouteFromPoint(indiv,
+                                           cube,
+                                           options,
+                                           j,
+                                           (vehiclesCount[v2] + offset) - j,
+                                           vehicleII.costPerKm,
+                                           v2);
 
-                        baseCpy[COST]  += costFitness(&fares[j],
-                                                      (vehiclesCount[v2] + pos2) - j,
-                                                      data,
-                                                      costPerKm,
-                                                      cube,
-                                                      vehicleIdx,
-                                                      indiv,
-                                                      options,
-                                                      NORMAL);
+                        /* Sum the old partial routes cost */
+                        for (int obj = 0; obj < NSGA2_OBJ; obj++) {
+                            oldValue[obj] += base[obj];
+                        }
 
-                        baseCpy[LOAD]  += loadFitness(&fares[j],
-                                                      (vehiclesCount[v2] + pos2) - j,
-                                                      data,
-                                                      costPerKm,
-                                                      cube,
-                                                      vehicleIdx,
-                                                      indiv,
-                                                      options,
-                                                      NORMAL);
-
+                        /* Create temporary individual to compute the edge switching*/
                         Individual tmp(data.plannedElements().size() + 1);
 
-                        //switch
-                        swapEdge(indiv, tmp, v, v2, i + 1, j + 1):
-                            sortFaresByTimestamp(tmp, j, (vehiclesCount[v2] + pos2) - j );
-                        sortFaresByTimestamp(tmp, i, (vehiclesCount[v] + pos) - i);
+                        /* Switch the edges */
+                        swapEdge(indiv, tmp, v, v2, i + 1, j + 1);
+                        sortFaresByTimestamp(tmp, j, (vehiclesCount[v2] + offset) - j );
+                        sortFaresByTimestamp(tmp, i, (vehiclesCount[v] + offsetBase) - i);
 
-                        newValue[DELAY] = delayFitness();
-                        newValue[COST] = costFitness();
-                        newValue[LOAD] = loadFitness();
-                        // and j
-                        newValue[DELAY] += delayFitness();
-                        newValue[COST] += costFitness();
-                        newValue[LOAD] += loadFitness();
+                        /* Compute the new cost of the base route from its changing point */
+                        auto tmpVecBase = vehicleResponsible(tmp, i);
+                        Fitness newValueBase = evaluateRouteFromPoint(tmp, cube, options, i,
+                                               (vehiclesCount[tmpVecBase] + offset) - i, vehicleI.costPerKm, v);
 
-                        auto res = checkDominanceHost(newValue, baseCpy, 3);
+                        /* Compute the new cost of the second route from its changing point */
+                        auto tmpVec = vehicleResponsible(tmp, j);
+                        Fitness newValue = evaluateRouteFromPoint(tmp, cube, options, j,
+                                           (vehiclesCount[tmpVec] + offset) - j, vehicleII.costPerKm, v2);
+
+                        /* Sum the new partial routes cost */
+                        for (int obj = 0; obj < NSGAII_OBJ; obj++) {
+                            newValue[obj] += newValueBase[obj];
+                        }
+
+                        /* Compare the sums of the old and new partial cost route */
+                        auto res = checkDominanceHost(newValue, oldValue, NSGA2_OBJ);
 
                         if (res == 1) {
                             indiv = tmp;
+                            improvement = true;
                         }
 
-                        // Compute old cost of the two route
-                        // Compute old delay
-                        // Compute old capacity
-
-
                     }
-                    pos2 += indiv.vehiclesCount[v2];
+                    offset += indiv.vehiclesCount[v2];
                 }
             }
         }
-        pos += indiv.vehiclesCount[v];
+        offsetBase += indiv.vehiclesCount[v];
     }
 }
 
